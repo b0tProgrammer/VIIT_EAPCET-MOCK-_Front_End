ef37e13a666dce63b7374bba67ba58fdd4b82150
mohit <s.mohithkumar1511@gmail.com>
2025-11-02 10:31:33 +0530

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+function NavBar() {
+    return (
+        <nav className="bg-blue-600 p-4 text-white">
+            <h1 className="text-2xl font-bold">VIIT's EAPCET Mock Test</h1>
+        </nav>
+    );
+}
+
+export default NavBar;
